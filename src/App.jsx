9aa1668643451dd5d493420ea380342a9006c577
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2023-12-26 01:25:31 +0600

    finally done

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import TodoTextField from './components/TodoTextField'
 function App() {
 
   return (
-    <div className=' bg-black w-full h-screen'>
-      <div className=' bg-gray-700 max-w-xl mx-auto p-4 rounded-lg '>
+    <div className=' bg-black w-full h-screen flex items-center justify-center'>
+      <div className=' bg-gray-700 max-w-xl mx-auto p-4 rounded-lg min-w-[400px] '>
         <TodoTextField />
         <MapList/>
       </div>
